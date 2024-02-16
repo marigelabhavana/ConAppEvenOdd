568a81a128ca2ae5ea8ec7e92a4398ec992c4cee
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-16 09:32:51 +0530

    all files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppEvenOdd;
+
+Console.WriteLine("Welcome to Number System !");
+Console.WriteLine("*** Even Numbers ***");
+NumberSystem.EvenNum();
+Console.WriteLine("*** Odd Numbers ***");
+NumberSystem.OddNum();
